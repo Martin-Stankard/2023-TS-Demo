648b148e38db9bcf57c16435fbe97b9edb514f39
b <martin.stankard@hotmail.com>
2022-09-29 13:57:51 -0700

    Updates

---
+++ precache-manifest.bddc1b64d947f3a6f0d11af56aadbddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f49f5011c0db15a5e7",
+    "url": "/Martin-Stankard/2023-ts-demo/static/css/main.2cedc718.chunk.css"
+  },
+  {
+    "revision": "d2f49f5011c0db15a5e7",
+    "url": "/Martin-Stankard/2023-ts-demo/static/js/main.d2f49f50.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Martin-Stankard/2023-ts-demo/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/Martin-Stankard/2023-ts-demo/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "d31c155eddd46fb776d3",
+    "url": "/Martin-Stankard/2023-ts-demo/static/js/runtime~main.d31c155e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Martin-Stankard/2023-ts-demo/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8a66e2dd1f7d5b62576979f32d6599fa",
+    "url": "/Martin-Stankard/2023-ts-demo/index.html"
+  }
+];
