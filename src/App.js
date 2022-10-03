b1ec93f737e604a2b1389b71456c5b5ba1f6a013
b <martin.stankard@hotmail.com>
2022-10-03 09:56:02 -0700

    force a deconflict test bits added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Some change to be seen!!!!!
+          Edit <code>src/App.js</code> and save to reload. Force a deconflict here form main. Some change to be seen!!!!!
         </p>
         <a
           className="App-link"
