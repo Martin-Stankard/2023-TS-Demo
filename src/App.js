81811a0f6bfca8ae2bb53da77788f5314d1d2d73
b <martin.stankard@hotmail.com>
2022-10-03 14:51:27 -0700

    change button name for intermittent working app

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const PokemonRow = ({ pokemon, onClick }) => (
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
       <td>
-        <button onClick={() => onClick(pokemon)}>More Information</button>
+        <button onClick={() => onClick(pokemon)}>SEL</button>
       </td>
     </tr>
   </>
